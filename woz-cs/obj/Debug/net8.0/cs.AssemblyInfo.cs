b27eb2baa3fcef48ab80e5b2bb41e28f5ceda4a7
Selma <selma.larsen@icloud.com>
2025-11-07 11:46:23 +0100

    space

--- woz-cs/obj/Debug/net8.0/cs.AssemblyInfo.cs
+++ woz-cs/obj/Debug/net8.0/cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dacbdfaf8820e170101534fef3c1663a35dbe6e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7bc40bf2576ad51d88c46104551bb8845648c4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
