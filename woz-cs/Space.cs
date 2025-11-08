94d3bd5b154f641143aa5470b88957da28f0028c
Jaevs <jeppedeleuran@yahoo.dk>
2025-11-08 17:48:04 +0100

    added .gitignore file

--- woz-cs/Space.cs
+++ woz-cs/Space.cs
@@ -1,7 +1,7 @@
 /* Space class for modeling spaces (rooms, caves, ...)
  */
-
 class Space : Node {
+
   public Space (String name) : base(name)
   {
   }
@@ -10,9 +10,29 @@ class Space : Node {
   {
     string description = "";
     string location = name;
-    if(location == "By")
+    if(location == "by")
     {
-      description = "Byen er i en forfalden tilstand";
+      description = "Du er borgermester for den afrikanske landsby, Broke-Block. Landsbyens har været hårdt ramt af tørke, hungersnød og naturkatastrofer. Byens huse er utætte og ødelagte, de har brug for en kærlig hånd. Vil du vælge at blive i byen for at snakke med borgerne? Eller vil du vandre ud i vildnis for at finde byggemateriale?";
+    }
+
+    if(location == "forfaldenby")
+    {
+      description = "Du vælger at blive i landsbyen. På vej hen til rådhuset går du forbi en lokal nabolag. Husene har stærkt brug for hjælp, da byggningerne er forfalden. En lokal beboer, Kofi går op til dig med to forskellige ting. Vil du vælge om feje støv og sten væk fra vejene? Eller modtag hammer, søm og træ til at bygge?";
+    }
+
+    if(location == "bygmateriale")
+    {
+      description = "Efter du har examineret husene i landsbyen, indser du hvor vigtigt det er at reparere borgernes huse. Så du samler et mindre hold og kører ud i vildmarken for at finde træ og andre potentielle byggematerialer. Efter i har samlet træ og sten i noget tid, spørger din cheufeur om i skal vende tilbage til landsbyen eller samle mere materiale. Vil du vandre længere væk og samle mere materiale eller vil du vende tilbage til byen?";
+    }
+
+    if(location == "landsbyaziz")
+    {
+      description = "Du har valgt at returnere tilbage til landsbyen. Tilbage ved husene møder du Aziz og Kofi, de hjælper dig med at reparere husene. Efterfølgende kommer du i snak med Aziz, som fortæller stolt at han er sygeplejerske hos det lokale hospital. Aziz fortæller sørgmodigt, at hospitalet har stærkt behov for ressourcer. Ignore Aziz anmodning for hjælp eller hjælpe Jørgen med at skaffe ressourcer til hospitalet.";
+    }
+
+    if(location == "hospital")
+    {
+      description = "Som borgermester, kontakter du andre nærlæggende byer og anstandser for at købe og forhandle om ressourcer til hospitalet. Efter hårde, men successfulde forhandlinger vender du tilbage til hospitalet med medicin og diverse ting. Tag medicin og ressourcer, og sælg det videre eller giv Aziz og hospitalet ressourcerne.";
     }
     else
     {
@@ -20,21 +40,22 @@ class Space : Node {
     }
     return description;
   }
-  
   public void Welcome () {
     Console.WriteLine("You are now at "+name);
     Console.WriteLine(GetDescription());
     HashSet<string> exits = edges.Keys.ToHashSet();
     Console.WriteLine("Current exits are:");
-    foreach (String exit in exits) {
+    foreach (String exit in exits)
+    {
       Console.WriteLine(" - "+exit);
     }
   }
-  
+
   public void Goodbye () {
   }
-  
-  public override Space FollowEdge (string direction) {
+
+  public override Space FollowEdge (string direction)
+  {
     return (Space) (base.FollowEdge(direction));
   }
 }
--- woz-cs/World.cs
+++ woz-cs/World.cs
@@ -5,19 +5,44 @@ class World {
   Space start;
   
   public World () {
-    Space start              = new Space("Start");
-    Space byggematerialer    = new Space("Byggematerialer");
-    Space by                 = new Space("By");
-    Space feje               = new Space("Feje");
-    Space byg                = new Space("Byg");
-    Space mere_tid_i_vildnis = new Space("mere_tid_i_vildnis");
+    Space start                      = new Space("start");
+    Space vildnis_byggematerialer    = new Space("vildnis");
+    Space by                         = new Space("by");
+    Space feje                       = new Space("feje");
+    Space byg_hus                    = new Space("byg_hus");
+    Space mere_tid_i_vildnis         = new Space("mere tid i vildnis");
+    Space hospital                   = new Space ("ressourcer til hospital");
+    Space ignorere_hospital          = new Space ("ignorere Jørgen");
+    Space skole                      = new Space ("byg skole");
+    Space bar                        = new Space ("byg bar");
+    Space samarbejde_med_bønder      = new Space ("samarbejde med bønder");
+    Space fortsæt_uden_samarbejde    = new Space ("fortsæt uden samarbejde");
+    Space byg_dæmninger              = new Space ("byg dæmninger");
+    Space ignorere_floden            = new Space ("ignorere floden");
+    Space sælge_råvarer              = new Space ("sælge råvarer");
+    Space behold_råvarer             = new Space ("behold råvarer");
+    Space forbedre_veje              = new Space ("forbedre veje");
+    Space behold_veje                = new Space ("behold veje");
+
     
-    start.AddEdge("byggematerialer", byggematerialer);
-    start.AddEdge("By", by);
+    start.AddEdge("vildnis", vildnis_byggematerialer);
+    start.AddEdge("by", by);
     by.AddEdge("feje", feje);
-    by.AddEdge("Byg", byg);
-    byggematerialer.AddEdge("Byg", byg);
-    byggematerialer.AddEdge("mere_tid_i_vildnis", mere_tid_i_vildnis);
+    by.AddEdge("byg_hus", byg_hus);
+    vildnis_byggematerialer.AddEdge("byg_hus", byg_hus);
+    vildnis_byggematerialer.AddEdge("mere tid i vildnis", mere_tid_i_vildnis);
+    byg_hus.AddEdge("ressourcer til hospital", hospital);
+    byg_hus.AddEdge("ignorere Jørgen", ignorere_hospital);
+    hospital.AddEdge("byg skole", skole);
+    hospital.AddEdge("byg bar", bar);
+    skole.AddEdge("samarbejde med bønder", samarbejde_med_bønder);
+    skole.AddEdge("fortsæt uden samarbejde", fortsæt_uden_samarbejde);
+    samarbejde_med_bønder.AddEdge("byg dæmninger", byg_dæmninger);
+    samarbejde_med_bønder.AddEdge("ignorere floden", ignorere_floden);
+    byg_dæmninger.AddEdge("sælge råvarer", sælge_råvarer);
+    byg_dæmninger.AddEdge("behold råvarer", behold_råvarer);
+    sælge_råvarer.AddEdge("forbedre veje", forbedre_veje);
+    sælge_råvarer.AddEdge("behold veje", behold_veje);
     
     this.start = start;
   }
