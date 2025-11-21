3f83737d742fb000f7aa9f89ffbdd963ed8d38eb
Caitlin Van <0caitlinvan0@gmail.com>
2025-11-21 08:09:27 +0100

    Update GameOverScreen.cs

--- woz-cs/UIManager/GameOverScreen.cs
+++ woz-cs/UIManager/GameOverScreen.cs
@@ -8,21 +8,21 @@
     //foreach(string line in ascii){
 
         //WriteCentered(line);
-    }
+    //}
     //Console.WriteLine();
-}
-}
+//}
+//}
 
 // Print each line centered
 //foreach (string line in ascii)
-{
+//{
     //WriteCentered(line);
     //Console.WriteLine();
     //Console.ReadLine();
-}
+//}
 
 //public static class GameOverScreen
-{
+//{
     //public static readonly string[] Art = {
       //@"
 
