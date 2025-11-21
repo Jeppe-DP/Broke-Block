f148d883f7d02dc3dc36f20713d72e66b62d36da
CV <0caitlinvan0@gmail.com>
2025-11-21 08:04:05 +0100

    add UI folder

---
+++ woz-cs/UIManager/GameOverScreen.cs
@@ -0,0 +1,38 @@
+//Print ASCII style Game Over screen.
+
+//public void WriteGameOver (string[] ascii){
+
+    //Clear(); //clears console screen.
+
+
+    //foreach(string line in ascii){
+
+        //WriteCentered(line);
+    }
+    //Console.WriteLine();
+}
+}
+
+// Print each line centered
+//foreach (string line in ascii)
+{
+    //WriteCentered(line);
+    //Console.WriteLine();
+    //Console.ReadLine();
+}
+
+//public static class GameOverScreen
+{
+    //public static readonly string[] Art = {
+      //@"
+
+
+
+
+
+
+
+
+      "
+    };
+}
